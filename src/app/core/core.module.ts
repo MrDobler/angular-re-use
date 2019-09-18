2e0283afbca88435ca9dab7a7c633bd56ccb5f4b
MrDobler <arthur.dobler@gmail.com>
2019-09-18 20:41:09 -0300

    Extraindo o componente de navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { InMemoryDatabase } from '../in-memory-database';
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
     declarations: [
+        NavBarComponent
     ],
     imports: [
         CommonModule,
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        RouterModule,
         HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
     ],
     exports: [
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        NavBarComponent
     ]
 })
 export class CoreModule {}
